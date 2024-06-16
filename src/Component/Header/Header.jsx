790e02f156e0b0c5fd29011d4190e61f4bb93f25
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-06-16 12:44:58 +0300

    completed header section

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+const Header = () =>{
+    return(
+        <>
+        <header className='header'>
+         <div className="header-container">
+            <h1>GITHUB FINDER</h1>
+            <div className="header-link">
+                <a href="https://github.com/AbdiIbrahimAdan">By Abdi Ibrahim</a>
+            </div>
+         </div>
+        </header>
+        </>
+    );
+};
+export default Header;
